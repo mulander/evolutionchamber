725070d36455f360fa0db74f5c1d08f3d4fb1a68
netprobe <netprobe@7f9e2161-bb24-0786-4dc4-ea047be1a2ce>
2010-10-30 15:57:49 +0000

    Work on Issue #30 - Created a EcConstants class. Table populated with values from the constants. The next logical step is to tie the type and name field of the target table together. The list of choices in the name combo box should depend on the value of the type.

---
+++ branches/NewGUI/EvolutionChamber/src/java/com/fray/evo/EcConstants.java
@@ -0,0 +1,143 @@
+package com.fray.evo;
+
+public class EcConstants {
+	
+	public static String UNIT = "Unit";
+	public static String UPGRADE = "Upgrade";
+	public static String STRUCTURE = "Structure";
+	
+	public static String[] TYPES = {
+		 UNIT
+		,UPGRADE
+		,STRUCTURE
+	};
+	// Units
+	public static String DRONES = "Drones";
+	public static String OVERLORDS = "Overlords";
+	public static String OVERSEERS = "Overseers";
+	public static String QUEENS = "Queens";
+	public static String ZERGLINGS = "Zerglings";
+	public static String BANELINGS = "Banelings";
+	public static String ROACHES = "Roaches";
+	public static String HYDRALISKS = "Hydralisks";
+	public static String INFESTORS = "Infestors";
+	public static String MUTALISKS = "Mutalisks";
+	public static String ULTRALISKS = "Ultralisks";
+	public static String CORRUPTORS = "Corruptors";
+	public static String BROODLORDS = "Broodlords";
+	// All units
+	public static String[] UNITS = {
+		 DRONES
+		,OVERLORDS
+		,OVERSEERS
+		,QUEENS
+		,ZERGLINGS
+		,BANELINGS
+		,ROACHES
+		,HYDRALISKS
+		,INFESTORS
+		,MUTALISKS
+		,CORRUPTORS
+		,BROODLORDS
+	};
+	// Upgrades
+	public static String PNEUMATIZED_CARAPACE = "Pneumatized Carapace";
+	public static String VENTRAL_SACKS = "Ventral Sacks";
+	public static String METABOLIC_BOOST = "Metabolic Boost";
+	public static String ADRENAL_GLANDS = "Adrenal Glands";
+	public static String CENTRIFUGAL_HOOKS = "Centrifugal Hooks";
+	public static String GLIAL_RECONSTITUTION = "Glial Reconstitution";
+	public static String TUNNELING_CLAWS = "Tunneling Claws";
+	public static String GROOVED_SPINES = "Grooved Spines";
+	public static String NEURAL_PARASITE = "Neural Parasite";
+	public static String PATHOGEN_GLANDS = "Pathogen Glands";
+	public static String CHITINOUS_PLATING = "Chitinous Plating";
+	public static String MELEE = "Melee";
+	public static String MELEE_1 = MELEE + " +1";
+	public static String MELEE_2 = MELEE + " +2";
+	public static String MELEE_3 = MELEE + " +3";
+	public static String MISSILE = "Missile";
+	public static String MISSILE_1 = MISSILE + " +1";
+	public static String MISSILE_2 = MISSILE + " +2";
+	public static String MISSILE_3 = MISSILE + " +3";
+	public static String CARAPACE = "Carapace";
+	public static String CARAPACE_1 = CARAPACE + " +1";
+	public static String CARAPACE_2 = CARAPACE + " +2";
+	public static String CARAPACE_3 = CARAPACE + " +3";
+	public static String FLYER = "Flyer";
+	public static String FLYER_ATTACK = FLYER + " Attack";
+	public static String FLYER_ATTACK_1 = FLYER_ATTACK + " +1";
+	public static String FLYER_ATTACK_2 = FLYER_ATTACK + " +2";
+	public static String FLYER_ATTACK_3 = FLYER_ATTACK + " +3";
+	public static String FLYER_ARMOR = FLYER + " Armor";
+	public static String FLYER_ARMOR_1 = FLYER_ARMOR + " +1";
+	public static String FLYER_ARMOR_2 = FLYER_ARMOR + " +2";
+	public static String FLYER_ARMOR_3 = FLYER_ARMOR + " +3";
+	// All Upgrades
+	public static String[] UPGRADES = {
+		 PNEUMATIZED_CARAPACE
+		,VENTRAL_SACKS
+		,METABOLIC_BOOST
+		,ADRENAL_GLANDS
+		,CENTRIFUGAL_HOOKS
+		,GLIAL_RECONSTITUTION
+		,TUNNELING_CLAWS
+		,GROOVED_SPINES
+		,NEURAL_PARASITE
+		,PATHOGEN_GLANDS
+		,CHITINOUS_PLATING
+		,MELEE_1
+		,MELEE_2
+		,MELEE_3
+		,MISSILE_1
+		,MISSILE_2
+		,MISSILE_3
+		,CARAPACE_1
+		,CARAPACE_2
+		,CARAPACE_3
+		,FLYER_ATTACK_1
+		,FLYER_ATTACK_2
+		,FLYER_ATTACK_3
+		,FLYER_ARMOR_1
+		,FLYER_ARMOR_2
+		,FLYER_ARMOR_3
+	};
+	// Buildings
+	public static String BASES = "Bases";
+	public static String LAIRS = "Lairs";
+	public static String HIVES = "Hives";
+	public static String GAS_EXTRACTORS = "Gas Extractors";
+	public static String EVOLUTION_CHAMBERS = "Evolution Chambers";
+	public static String SPINE_CRAWLERS = "Spine Crawlers";
+	public static String SPORE_CRAWLERS = "Spore Crawlers";
+	public static String SPAWNING_POOLS = "Spawning Pools";
+	public static String BANELINGS_NEST = "Banelings Nest";
+	public static String ROACH_WARRENS = "Roach Warrens";
+	public static String HYDRALISK_DENS = "Hydralisk Dens";
+	public static String INFESTATION_PITS = "Infestation Pits";
+	public static String SPIRES = "Spires";
+	public static String NYDUS_NETWORKS = "Nydus Networks";
+	public static String NYDUS_WORMS = "Nydus Worms";
+	public static String ULTRALISK_CAVERNS = "Ultralisk Caverns";
+	public static String GREATER_SPIRES = "Greater Spires";
+	// All Buildings
+	public static String[] STRUCTURES = {
+		 BASES
+		,LAIRS
+		,HIVES
+		,GAS_EXTRACTORS
+		,EVOLUTION_CHAMBERS
+		,SPINE_CRAWLERS
+		,SPORE_CRAWLERS
+		,SPAWNING_POOLS
+		,BANELINGS_NEST
+		,ROACH_WARRENS
+		,HYDRALISK_DENS
+		,INFESTATION_PITS
+		,SPIRES
+		,NYDUS_NETWORKS
+		,NYDUS_WORMS
+		,ULTRALISK_CAVERNS
+		,GREATER_SPIRES
+	};
+}
