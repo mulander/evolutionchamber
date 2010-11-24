e08c9a7b486228329c260b403a72f6b320e602d4
mike.angstadt <mike.angstadt@7f9e2161-bb24-0786-4dc4-ea047be1a2ce>
2010-11-24 02:08:57 +0000

    Fixed issue 154: Auto-updater now checks the downloaded file against a checksum to verify file integrity.  It will also not download the updated version if it was downloaded before. Fixed issue 157: The main window no longer hides the auto-updater's progress bar when an update is being performed.

--- trunk/EvolutionChamber/test/java/com/fray/evo/util/EcAutoUpdateTest.java
+++ trunk/EvolutionChamber/test/java/com/fray/evo/util/EcAutoUpdateTest.java
@@ -1,18 +1,24 @@
 package com.fray.evo.util;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Tests the EcAutoUpdate class.
+ * 
  * @author mike.angstadt
- *
+ * 
  */
 public class EcAutoUpdateTest {
 	/**
 	 * Tests what happens when there is a newer version available.
+	 * 
 	 * @throws Exception
 	 */
 	@Test
@@ -20,7 +26,11 @@ public class EcAutoUpdateTest {
 		//version "0013" is an old version so there should be a newer one available
 		String oldVersion = "0013";
 		EcAutoUpdate auto = new EcAutoUpdate(oldVersion);
+		auto.addPropertyChangeListener(new DisplayDownloadProgress());
 		String latestVersion = auto.getLatestVersion();
+		File file = new File("evolutionchamber-version-" + latestVersion + ".jar");
+		file.delete(); //delete the file if it exists from a previous test run
+
 		Assert.assertTrue(auto.isUpdateAvailable());
 		Assert.assertTrue(Integer.parseInt(oldVersion) < Integer.parseInt(latestVersion));
 
@@ -30,8 +40,26 @@ public class EcAutoUpdateTest {
 		Assert.assertTrue(auto.isUpdating());
 
 		//file should have been downloaded to the current working directory
-		File file = new File("evolutionchamber-version-" + latestVersion + ".jar");
 		Assert.assertTrue(file.exists());
+		long lastModified = file.lastModified();
+
+		//run the update again
+		//since the file was already downloaded, it should not re-download it
+		auto = new EcAutoUpdate(oldVersion);
+		auto.doInBackground();
+		Assert.assertEquals(lastModified, file.lastModified()); //the file should not have changed
+
+		//run the update again
+		//if a file exists with the same name of the JAR file, but is *not* the JAR file, then it should notice this and re-download the JAR file
+		PrintWriter out = new PrintWriter(new FileOutputStream(file));
+		out.print("This is just some text file that happens to have the same name as the Evolution Chamber JAR.");
+		out.close();
+		lastModified = file.lastModified();
+		auto = new EcAutoUpdate(oldVersion);
+		auto.addPropertyChangeListener(new DisplayDownloadProgress());
+		auto.doInBackground();
+		Assert.assertTrue(lastModified < file.lastModified());
+
 		file.delete();
 	}
 
@@ -51,5 +79,33 @@ public class EcAutoUpdateTest {
 		//there should be no update available
 		auto = new EcAutoUpdate(latestVersion);
 		Assert.assertFalse(auto.isUpdateAvailable());
+	}
+
+	/**
+	 * Displays the download progress of an update to stdout.
+	 * 
+	 * @author mike.angstadt
+	 * 
+	 */
+	private class DisplayDownloadProgress implements PropertyChangeListener {
+		private boolean first = true;
+
+		public void propertyChange(PropertyChangeEvent evt) {
+			if ("progress".equals(evt.getPropertyName())) {
+				if (first) {
+					first = false;
+					System.out.print("Downloading ... ");
+				}
+				int value = (Integer) evt.getNewValue();
+				if (value % 10 == 0) {
+					System.out.print(value + "%");
+					if (value == 100) {
+						System.out.println();
+					} else {
+						System.out.print(" ... ");
+					}
+				}
+			}
+		}
 	}
 }
