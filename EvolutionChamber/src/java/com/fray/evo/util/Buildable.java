960c9eef455317af15fbe8958fc4acd23d2f66d9
domagala.lukas <domagala.lukas@gmail.com>
2010-11-30 23:19:06 +0000

    added buildable interface for anything that can be built. will fill the unit/building/upgrade library and change all references to costs and names to use these

---
+++ EvolutionChamber/src/java/com/fray/evo/util/Buildable.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.fray.evo.util;
+
+/**
+ *
+ * @author Cyrik
+ */
+public interface Buildable {
+    public int getMinerals();
+    public int getGas();
+    public double getTime();
+
+}
