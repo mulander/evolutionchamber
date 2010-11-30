e4f8a97432b423274aeadd4fd6a4a605fdbdf5de
domagala.lukas <domagala.lukas@gmail.com>
2010-11-30 23:23:13 +0000

    added all units and an upgrade

---
+++ EvolutionChamber/src/java/com/fray/evo/util/UpgradeLibrary.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.fray.evo.util;
+
+/**
+ *
+ * @author Cyrik
+ */
+public class UpgradeLibrary {
+    public static Upgrade MetabolicBoost = new Upgrade("MetabolicBoost", 100, 100, 110);
+
+}
