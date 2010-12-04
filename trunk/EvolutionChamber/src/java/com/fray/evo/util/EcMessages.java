bcb8bf7c766c9b5f220d62a6e940bab3439590c8
mike.angstadt <mike.angstadt@7f9e2161-bb24-0786-4dc4-ea047be1a2ce>
2010-12-04 02:10:39 +0000

    Removed code from EcMessages that allowed properties to be referenced inside of other properties because it was causing problems with i18n.  Updated unit test to scan all source code for valid messages (instead of just the GUI code).

--- trunk/EvolutionChamber/src/java/com/fray/evo/util/EcMessages.java
+++ trunk/EvolutionChamber/src/java/com/fray/evo/util/EcMessages.java
@@ -2,10 +2,7 @@ package com.fray.evo.util;
 
 import java.text.MessageFormat;
 import java.util.Locale;
-import java.util.MissingResourceException;
 import java.util.ResourceBundle;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Retrieves string messages from a ResourceBundle properties file for i18n.
@@ -14,12 +11,6 @@ import java.util.regex.Pattern;
  * 
  */
 public class EcMessages {
-	/**
-	 * Regex used to look for references to other properties. For example:
-	 * {foo.bar}.
-	 */
-	private static final Pattern propRefRegex = Pattern.compile("\\{(.*?)\\}");
-
 	private final ResourceBundle messages;
 
 	/**
@@ -39,20 +30,7 @@ public class EcMessages {
 	 * @return the message
 	 */
 	public String getString(String key) {
-		String message = messages.getString(key);
-
-		//handle references to other properties
-//		Matcher m = propRefRegex.matcher(message);
-//		while (m.find()) {
-//			try {
-//				String refMessage = getString(m.group(1).trim());
-//				message = message.replace(m.group(0), refMessage);
-//			} catch (MissingResourceException e) {
-//				//ignore references to non-existant properties and to argument references ("{0}")
-//			}
-//		}
-
-		return message;
+		return messages.getString(key);
 	}
 
 	/**
