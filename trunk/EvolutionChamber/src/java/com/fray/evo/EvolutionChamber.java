4e1941a4dd252871465f1682b5858819849e7bc3
fritley <fritley@7f9e2161-bb24-0786-4dc4-ea047be1a2ce>
2011-01-26 03:20:15 +0000

    V0024

--- trunk/EvolutionChamber/src/java/com/fray/evo/EvolutionChamber.java
+++ trunk/EvolutionChamber/src/java/com/fray/evo/EvolutionChamber.java
@@ -41,7 +41,7 @@ public final class EvolutionChamber
 	/**
 	 * The version of EvolutionChamber.
 	 */
-	public static final String	VERSION = "0023";
+	public static final String	VERSION = "0024";
 
 	/**
 	 * The seed file.
