975d0a5452e8bf821e9d4e00a22756c9f2df5e1a
netprobe@gmail.com <netprobe@gmail.com@7f9e2161-bb24-0786-4dc4-ea047be1a2ce>
2011-01-27 07:18:59 +0000

    Fixed issue #191 - wrong version number in the last release.

--- trunk/EvolutionChamber/src/java/com/fray/evo/EvolutionChamber.java
+++ trunk/EvolutionChamber/src/java/com/fray/evo/EvolutionChamber.java
@@ -41,7 +41,7 @@ public final class EvolutionChamber
 	/**
 	 * The version of EvolutionChamber.
 	 */
-	public static final String	VERSION = "0024";
+	public static final String	VERSION = "0026";
 
 	/**
 	 * The seed file.
